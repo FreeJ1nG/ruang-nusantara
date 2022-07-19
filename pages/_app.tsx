175d7ce562d11e0474801f166938f2cd629f647e
Andrew Jeremy <Andrewjeremy12345@gmail.com>
2022-07-19 17:49:37 +0700

    added title, app logo, provider, navbar, and footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,29 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+
+import type { AppProps } from "next/app";
+import Footer from "../components/Footer/Footer";
+import Head from "next/head";
+import { IndoContext } from "../context/IndoContext";
+import Navbar from "../components/Navbar/Navbar";
+import { useState } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	const [indo, setIndo] = useState(true);
+
+	return (
+		<IndoContext.Provider value={indo}>
+			<Head>
+				<title>Ruang Nusantara</title>
+				<link rel="icon" href="/logo.png" />
+			</Head>
+			<div className="relative flex flex-col text-white">
+				<Navbar indo={indo} setIndo={setIndo} />
+				<div className="h-24" />
+				<Component {...pageProps} />
+				<Footer />
+			</div>
+		</IndoContext.Provider>
+	);
 }
 
-export default MyApp
+export default MyApp;
