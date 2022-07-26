377f2432d33fa340b930f61f28da89f84e4eae5f
Andrew Jeremy <Andrewjeremy12345@gmail.com>
2022-07-27 03:50:44 +0700

    fixed footer and made it responsive

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,136 +1,163 @@
 import { FACEBOOK_ICON, INSTAGRAM_ICON, YOUTUBE_ICON } from "./constants";
 
-import Button from "../Button/Button";
 import { FC } from "react";
 import Image from "next/image";
 import TertiaryButton from "../Button/TertiaryButton";
 import { useRouter } from "next/router";
 
+const brownTitle: string = "font-bold text-sm md:text-lg text-lightBrown";
+
 const Footer: FC = () => {
 	const router = useRouter();
+
 	return (
-		<div className="bg-footer flex flex-col gap-y-20 py-3 pt-10">
+		<div className="bg-footer flex flex-col gap-y-5 md:gap-y-20 pb-5 pt-20">
 			<div className="flex flex-row mx-10 lg:mx-20">
-				<RuangNusantara />
-				<PricingAndGetHelp />
-				<FollowAndSocials />
-				<div className="hidden lg:flex w-1/4 justify-center items-center">
-					<LogoAndExplore />
+				<div className="hidden lg:flex lg:flex-col gap-y-10 w-1/3">
+					<Logo />
+					<FollowUs />
+					<h1 className="text-footerText text-lg">
+						<button className="underline text-sm sm:text-base md:text-lg">
+							Privacy Policies
+						</button>
+					</h1>
+				</div>
+				<div className="hidden sm:flex flex-row gap-x-5 w-full lg:w-2/3">
+					<div className="sm:w-1/3 flex flex-col gap-y-10">
+						<TopDestinations />
+					</div>
+					<div className="sm:w-1/3 flex flex-col gap-y-10">
+						<TravelInterests />
+						<HelpCenter />
+					</div>
+					<div className="sm:w-1/3 flex flex-col gap-y-10">
+						<Shop />
+						<AboutUs />
+					</div>
+				</div>
+				<div className="sm:hidden w-full flex flex-row">
+					<div className="w-1/2 flex flex-col gap-y-10">
+						<TopDestinations />
+						<HelpCenter />
+						<AboutUs />
+					</div>
+					<div className="w-1/2 flex flex-col gap-y-10">
+						<TravelInterests />
+						<Shop />
+					</div>
 				</div>
 			</div>
-			<div className="flex lg:hidden justify-center">
-				<LogoAndExplore />
+			<div className="flex flex-col gap-y-10 justify-center items-center lg:hidden">
+				<Logo />
+				<FollowUs />
+				<h1 className="text-footerText text-sm sm:text-base md:text-lg">
+					<button className="underline">Privacy Policies</button>
+				</h1>
 			</div>
-			<h1 className="font-normal text-base text-center">
+			<h1 className="TRAVEL INTERESTS-normal text-sm sm:text-base md:text-lg text-white text-center font-poppins">
 				©2022 Ruang Nusantara. All Rights Reserved.
 			</h1>
 		</div>
 	);
 };
 
-const LogoAndExplore: FC = () => {
+const FollowUs: FC = () => {
+	return (
+		<div className="flex flex-col gap-y-2">
+			<h1 className={`${brownTitle} text-center lg:text-left`}>Follow Us</h1>
+			<div className="flex flex-row gap-x-4">
+				<FACEBOOK_ICON />
+				<INSTAGRAM_ICON />
+				<YOUTUBE_ICON />
+			</div>
+		</div>
+	);
+};
+
+const Logo: FC = () => {
 	const router = useRouter();
 	return (
-		<div className="w-full flex flex-col items-center justify-center gap-y-10">
+		<div className="w-full flex flex-col items-center lg:items-start justify-center gap-y-10">
 			<div className="relative w-48 h-48">
 				<Image src="/logo.png" layout="fill" alt="Logo" />
 			</div>
-			<Button
-				label="EXPLORE NOW"
-				width={200}
-				height={50}
-				onClick={() => {
-					router.push("/explore");
-				}}
-			/>
+			<div className="flex flex-col gap-y-2">
+				<h1 className="font-serifpro text-yellowText font-bold text-3xl text-center lg:text-left">
+					Ruang Nusantara
+				</h1>
+				<h1 className="font-sanspro text-white text-sm text-center lg:text-left">
+					Explore Indonesia
+				</h1>
+			</div>
 		</div>
 	);
 };
 
-const FollowAndSocials: FC = () => {
+const Shop: FC = () => {
 	return (
-		<div className="w-1/3 lg:w-1/4 flex flex-col gap-y-20">
-			<div className="flex flex-col gap-y-2">
-				<h1 className="mb-3 font-bold text-base lg:text-xl text-lightBrown">
-					Follow Us
-				</h1>
-				<div className="flex flex-row gap-x-4">
-					<FACEBOOK_ICON />
-					<INSTAGRAM_ICON />
-					<YOUTUBE_ICON />
-				</div>
-			</div>
-			<div className="flex flex-col gap-y-2">
-				<h1 className="mb-3 font-bold text-base lg:text-xl text-lightBrown">
-					Contact Us
-				</h1>
-				<div className="flex flex-row items-center gap-x-4 text-lg text-footerText">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						className="h-6 w-6"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke="currentColor"
-						strokeWidth={2}
-					>
-						<path
-							strokeLinecap="round"
-							strokeLinejoin="round"
-							d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
-						/>
-					</svg>
-					<h1 className="overflow-hidden text-sm md:text-lg">
-						askruangnusantara@gmail.com
-					</h1>
-				</div>
-			</div>
+		<div className="flex flex-col gap-y-3">
+			<h1 className={brownTitle}>TRAVEL INTERESTS</h1>
+			<TertiaryButton label="Paintings" align="left" />
+			<TertiaryButton label="Sculpture" align="left" />
+			<TertiaryButton label="Fashion" align="left" />
+			<TertiaryButton label="Frozen Foods" align="left" />
+			<TertiaryButton label="Crafts" align="left" />
+			<TertiaryButton label="Non-English Guides" align="left" />
 		</div>
 	);
 };
 
-const PricingAndGetHelp: FC = () => {
+const AboutUs: FC = () => {
+	return (
+		<div className="flex flex-col gap-y-3">
+			<h1 className={brownTitle}>ABOUT US</h1>
+			<TertiaryButton label="About Ruang Nusantara" align="left" />
+			<TertiaryButton label="Press, Trade, and Advertising" align="left" />
+			<TertiaryButton label="Work For Us" align="left" />
+			<TertiaryButton label="Terms and Conditions" align="left" />
+		</div>
+	);
+};
+
+const TravelInterests: FC = () => {
 	const router = useRouter();
 	return (
-		<div className="w-1/3 lg:w-1/4 flex flex-col gap-y-20">
-			<div className="flex flex-col gap-y-2">
-				<h1 className="mb-3 font-bold text-base lg:text-xl text-lightBrown">
-					Pricing
-				</h1>
-				<TertiaryButton label="Overview" align="left" />
-				<TertiaryButton label="Affiliate Program" align="left" />
-				<TertiaryButton label="Promotions" align="left" />
-			</div>
-			<div className="flex flex-col gap-y-2">
-				<h1 className="mb-3 font-bold text-base lg:text-xl text-lightBrown">
-					Get Help
-				</h1>
-				<TertiaryButton label="FAQ" align="left" />
-				<TertiaryButton label="How To Purchase" align="left" />
-				<TertiaryButton label="Payment Options" align="left" />
-				<TertiaryButton label="Report Product" align="left" />
-			</div>
+		<div className="flex flex-col gap-y-3">
+			<h1 className={brownTitle}>TRAVEL INTERESTS</h1>
+			<TertiaryButton label="Art and Culture" align="left" />
+			<TertiaryButton label="Beaches" align="left" />
+			<TertiaryButton label="Culinary" align="left" />
+			<TertiaryButton label="Family Holidays" align="left" />
+			<TertiaryButton label="Fashion" align="left" />
+			<TertiaryButton label="Travel on Budget" align="left" />
+			<TertiaryButton label="Wildlife and Nature" align="left" />
 		</div>
 	);
 };
 
-const RuangNusantara: FC = () => {
+const HelpCenter: FC = () => {
+	return (
+		<div className="flex flex-col gap-y-3">
+			<h1 className={brownTitle}>HELP CENTER</h1>
+			<TertiaryButton label="Contact Us" align="left" />
+			<TertiaryButton label="FAQ" align="left" />
+		</div>
+	);
+};
+
+const TopDestinations: FC = () => {
 	const router = useRouter();
 	return (
-		<div className="w-1/3 lg:w-1/4 flex flex-col gap-y-2">
-			<h1 className="mb-3 font-bold text-base lg:text-xl text-lightBrown">
-				Ruang Nusantara
-			</h1>
-			<TertiaryButton
-				label="About Us"
-				onClick={() => router.push("/about")}
-				align="left"
-			/>
-			<TertiaryButton label="Services" align="left" />
-			<TertiaryButton label="Partnerships" align="left" />
-			<TertiaryButton label="Join Our Team" align="left" />
-			<TertiaryButton label="Press" align="left" />
-			<TertiaryButton label="Privacy Policy" align="left" />
-			<TertiaryButton label="Terms & Condition" align="left" />
+		<div className="flex flex-col gap-y-3">
+			<h1 className={brownTitle}>TOP DESTINATIONS</h1>
+			<TertiaryButton label="Bali" align="left" />
+			<TertiaryButton label="Labuan Bajo" align="left" />
+			<TertiaryButton label="Java" align="left" />
+			<TertiaryButton label="Sumatera" align="left" />
+			<TertiaryButton label="Kalimantan" align="left" />
+			<TertiaryButton label="Sulawesi" align="left" />
+			<TertiaryButton label="Papua" align="left" />
+			<TertiaryButton label="Maluku" align="left" />
 		</div>
 	);
 };
