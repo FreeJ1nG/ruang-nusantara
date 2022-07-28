ea54a4a44a5f2682ac3037bde15e3bda7fe10ac3
Andrew Jeremy <Andrewjeremy12345@gmail.com>
2022-07-28 09:56:13 +0700

    added constants for homepage

---
+++ pages/constants.ts
@@ -0,0 +1,42 @@
+export const DESTINATIONS = [
+	{
+		id: 0,
+		to: "/",
+		imageSrc: "/sumatera.png",
+		title: "Sumatera",
+		description:
+			"Located in the west part of Indonesia lorem ipsum lorem ipsum lorem ipsum",
+	},
+	{
+		id: 1,
+		to: "/",
+		imageSrc: "/kalimantan.png",
+		title: "Kalimantan",
+		description:
+			"Located in the west part of Indonesia lorem ipsum lorem ipsum lorem ipsum",
+	},
+	{
+		id: 2,
+		to: "/",
+		imageSrc: "/java_and_bali.png",
+		title: "Java & Bali",
+		description:
+			"Located in the west part of Indonesia lorem ipsum lorem ipsum lorem ipsum",
+	},
+	{
+		id: 3,
+		to: "/",
+		imageSrc: "/sulawesi.png",
+		title: "Sulawesi",
+		description:
+			"Located in the west part of Indonesia lorem ipsum lorem ipsum lorem ipsum",
+	},
+	{
+		id: 4,
+		to: "/",
+		imageSrc: "/papua.png",
+		title: "Papua",
+		description:
+			"Located in the west part of Indonesia lorem ipsum lorem ipsum lorem ipsum",
+	},
+];
