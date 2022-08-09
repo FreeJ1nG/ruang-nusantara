76f6831ca4ee168385800d0ef6b68f12f1646b6e
Andrew Jeremy <Andrewjeremy12345@gmail.com>
2022-08-09 20:40:45 +0700

    added: hover effect

--- components/MonthCard/index.tsx
+++ components/MonthCard/index.tsx
@@ -22,7 +22,7 @@ const MonthCard: FC<MonthCardProps> = ({
 	return (
 		<button
 			onClick={() => onClick()}
-			className="bg-white p-8 rounded-xl border-2 border-gray7 flex flex-col gap-y-5 items-center justify-center"
+			className="bg-white hover:bg-greenHover/30 transition-all duration-300 p-8 rounded-xl border-2 border-gray7 flex flex-col gap-y-5 items-center justify-center"
 		>
 			<div className="text-3xl font-medium font-ubuntu text-center">
 				{month}
